1feb5b4112051c0ad0a4594b3df2062309cb4d9e
kibethh <kibethh@gmail.com>
2023-02-02 11:08:04 +0300

    created respective repos

--- src/money/money.module.ts
+++ src/money/money.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { MoneyController } from './money.controller';
 import { MoneyService } from './money.service';
+import { Money } from './money.entity';
 
 @Module({
+  // Creating money repo
+  imports: [TypeOrmModule.forFeature([Money])],
   controllers: [MoneyController],
-  providers: [MoneyService]
+  providers: [MoneyService],
 })
 export class MoneyModule {}
--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { WalletsController } from './wallets.controller';
 import { WalletsService } from './wallets.service';
+import { Wallet } from './wallet.entity';
 
 @Module({
+  // Creating Wallet repository
+  imports: [TypeOrmModule.forFeature([Wallet])],
   controllers: [WalletsController],
-  providers: [WalletsService]
+  providers: [WalletsService],
 })
 export class WalletsModule {}
